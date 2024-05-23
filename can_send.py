0d85ca31d7aaf937331b5910ab7a8f377440b448
Anthony Han <ahan@envgo.com>
2024-05-23 10:48:10 -0400

    can_send back to using testbench.dbc

--- can_send.py
+++ can_send.py
@@ -41,7 +41,7 @@ class msg_sender():
 
 if __name__ == '__main__':
     bus = can.Bus(interface='udp_multicast', channel='239.0.0.1', port=10000, receive_own_messages=False)
-    db = cantools.database.load_file('../envgo/dbc/nv1_syscan.dbc')
+    db = cantools.database.load_file('../envgo/dbc/testbench.dbc')
 
     assert(isinstance(db, Database))
     
